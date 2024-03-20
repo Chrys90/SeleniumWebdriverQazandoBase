af8573f991fe98a161e622ec025603e7f06e2fc6
Chrystopher Gois <chrystophergois@hotmail.com>
2024-03-20 16:54:41 -0300

    Criação do projeto no github

---
+++ src/test/java/pages/CadastroUsuarioPage.java
@@ -0,0 +1,41 @@
+package pages;
+
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.interactions.Actions;
+import runner.RunCucumber;
+import support.Utils;
+
+import static support.Commands.*;
+
+public class CadastroUsuarioPage extends RunCucumber {
+
+    // elementos
+    private By campoNome = By.id("user");
+    private By campoEmail = By.id("email");
+    private By campoSenha = By.id("password");
+    private By botaoFazerCadastro = By.id("btnRegister");
+
+
+    // ações / funções / métodos
+    public void preencheNome(String nome) {
+        FillField(campoNome, nome);
+
+    }
+
+    public void preencheEmail(String email) {
+        FillField(campoEmail, email);
+    }
+
+    public void preencherSenha(String senha) {
+        FillField(campoSenha, senha);
+    }
+
+    public void cadastrarUsuario() {
+        clickElement(botaoFazerCadastro);
+    }
+
+    public void verificaCadastroSucesso() {
+        checkMessage(By.id("swal2-title"),"Cadastro realizado!");
+    }
+}
