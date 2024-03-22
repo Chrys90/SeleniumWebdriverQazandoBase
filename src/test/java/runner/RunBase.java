fc8491dfbb742c82dd2cdf73045da2650c9a2034
Chrystopher Gois <chrystophergois@hotmail.com>
2024-03-22 16:12:22 -0300

    criação da estrutura inicial da pipeline

--- src/test/java/runner/RunBase.java
+++ src/test/java/runner/RunBase.java
@@ -2,6 +2,7 @@ package runner;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.util.concurrent.TimeUnit;
@@ -24,6 +25,13 @@ public class RunBase {
             case "chrome":
                 driver = new ChromeDriver();
                 break;
+
+            case "chrome-ci":
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.addArguments("--headless");
+                driver = new ChromeDriver(chromeOptions);
+                break;
+
             case "firefox":
                 driver = new FirefoxDriver();
                 break;
